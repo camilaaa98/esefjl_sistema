84f502bc577e4c4c31eac8257f51d4f4abbafe70
Camila_Guevara <campguevara@gmail.com>
2026-05-06 06:20:54 -0500

    debug: check index.php content on Render

--- public/debug.php
+++ public/debug.php
@@ -1,8 +1,5 @@
 <?php
-echo "Render Debug Info:<br>";
-echo "Current File: " . __FILE__ . "<br>";
-echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
-echo "PHP Version: " . phpversion() . "<br>";
-echo "Config exists: " . (file_exists(__DIR__ . '/../app/config/config.php') ? 'YES' : 'NO') . "<br>";
-echo "Commit: " . date('Y-m-d H:i:s') . "<br>";
+echo "FILE CONTENT of index.php:<pre>";
+echo htmlspecialchars(file_get_contents(__DIR__ . '/index.php'));
+echo "</pre>";
 ?>
