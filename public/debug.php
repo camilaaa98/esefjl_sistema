77c700f6a72d820d77c54b878e20a4b5f9af36a9
Camila_Guevara <campguevara@gmail.com>
2026-05-06 06:24:15 -0500

    debug: reset opcache on Render

--- public/debug.php
+++ public/debug.php
@@ -1,4 +1,10 @@
 <?php
+if (function_exists('opcache_reset')) {
+    opcache_reset();
+    echo "OpCache Reset: SUCCESS<br>";
+} else {
+    echo "OpCache Reset: NOT AVAILABLE<br>";
+}
 echo "FILE CONTENT of index.php:<pre>";
 echo htmlspecialchars(file_get_contents(__DIR__ . '/index.php'));
 echo "</pre>";
